ca4942436aa2168f3dfec6ae611e48cd77e22c11
jmelote2909 <jmelote2909@iesmarquesdecomares.org>
2026-03-19 09:34:47 +0100

    ajustando

--- gym-app/app/workout/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function WorkoutLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="active" options={{ presentation: 'fullScreenModal' }} />
-    </Stack>
-  );
-}
